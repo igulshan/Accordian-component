fd2eacecf4251d2ae1face0ff6ab3ad9bf63226f
igulshan <ericmishra02@gmail.com>
2022-01-10 15:45:09 +0530

    mutiple Accordian

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordian from './accordian'
+
+
+const databook = [
+  {'title':'First Accordian',
+   'content':'a musical instrument played by stretching and squeezing   sounded by buttons or keys'},
+  {'title':'Second Accordian',
+   'content':'a musical instrument played by stretching and squeezing   sounded by buttons or keys'},
+  {'title':'Second Accordian',
+  'content':'a musical instrument played by stretching and squeezing   sounded by buttons or keys'}
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        databook.map((data)=>(
+          <Accordian data ={data}/>
+        ))
+      }
     </div>
   );
 }
