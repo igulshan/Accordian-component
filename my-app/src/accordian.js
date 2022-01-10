5e0052af691f602fc8e55485651fb3d909a31983
igulshan <ericmishra02@gmail.com>
2022-01-10 14:40:52 +0530

    updated

--- my-app/src/accordian.js
+++ my-app/src/accordian.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './accordian.css';
-import {useState} from 'react';
 
 function Accordian(props){
     const {databook} = props;
-    const [isActive, setIsActive] = useState(false);
     return(
         <>
             <div className='container'>
             <div className='head'>
             <div className='title'>{databook.title}</div>
-            <div className='btn' onClick={() => setIsActive(!isActive)}>{isActive ? 'Hide' : 'Show'}</div>
             </div>
-            {isActive && <p className='data'>{databook.text }</p>}
+            <p className='data'>{databook.text }</p>
             </div>
         
         </>
