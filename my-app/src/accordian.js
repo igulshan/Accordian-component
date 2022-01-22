bdbca9162f8a8389795e08896725f30e4f39334a
igulshan <ericmishra02@gmail.com>
2022-01-22 16:22:15 +0530

    updated , toogle button working

--- my-app/src/accordian.js
+++ my-app/src/accordian.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './accordian.css';
+import {useState} from 'react';
 
 function Accordian(props){
+    const [isOpen,setOpen]=useState(false);
     const {databook} = props;
     return(
         <>
             <div className='container'>
             <div className='head'>
             <div className='title'>{databook.title}</div>
+            <div className='show' onClick={()=>setOpen(!isOpen)}>{isOpen ? 'Hide':'Show'}</div>
             </div>
-            <p className='data'>{databook.text }</p>
+            {isOpen && <p className='data'>{databook.text }</p>}
+            
             </div>
         
         </>
